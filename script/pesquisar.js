648eec37e80718d372dd66aece06a0f97e820ffe
Nicolas Bueno <nickibueno@gmail.com>
2022-10-18 15:20:51 -0300

    atualizações

---
+++ script/pesquisar.js
@@ -0,0 +1,20 @@
+const teclas = document.querySelectorAll(".grid")
+const input = document.querySelector(".campo")
+const linhas = document.querySelector(".linhas")
+
+function digitaCampo(event){
+    const elemento = event.target
+    if(elemento.innerText === "CLR"){
+        input.innerText = "" 
+    }
+    else{
+        input.innerText += elemento.innerText
+        linhas.style.display="flex";
+    }
+   
+}
+
+teclas.forEach((item)=>{
+    item.addEventListener("click", digitaCampo)
+})
+
