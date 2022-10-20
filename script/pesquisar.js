1efac550898a34a0a690a79cdd63f7b29f4732d1
Nicolas Bueno <nickibueno@gmail.com>
2022-10-20 14:46:48 -0300

    iniciando tela confirmações

--- script/pesquisar.js
+++ script/pesquisar.js
@@ -1,6 +1,7 @@
 const teclas = document.querySelectorAll(".grid")
 const input = document.querySelector(".campo")
 const linhas = document.querySelector(".linhas")
+const btnConfirmar = document.querySelector(".confirmar")
 
 function digitaCampo(event){
     const elemento = event.target
@@ -19,3 +20,10 @@ teclas.forEach((item)=>{
     item.addEventListener("click", digitaCampo)
 })
 
+function verificaCampo(){
+  if(input.innerText === "05000"){
+    btnConfirmar.setAttribute("href","./confirma.html")
+  }
+}
+
+btnConfirmar.addEventListener("click", verificaCampo)
